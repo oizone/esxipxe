7495d811fe065aab345306e8eaa9301e3c902c46
Onni Rautanen <oizone@oizone.net>
2020-07-22 21:17:36 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/plain')])
-    file=open("tftp"+env[PATH_INFO],'r')
+    file=open("tftp"+env['PATH_INFO'],'r')
     return [file.read()]
