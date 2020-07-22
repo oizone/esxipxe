a0814ad9524cc481f739c87743566d9876935f7c
Onni Rautanen <oizone@oizone.net>
2020-07-22 22:21:15 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/plain')])
-    file=open("/tftp"+env['PATH_INFO'],'r')
-    return [file.read()]
+    if(env['PATH_INFO'].split('/')[1]=="cd"):
+        return ["JOO CEEDEE"]
+    else:
+        file=open("/tftp"+env['PATH_INFO'],'r')
+        return [file.read()]
