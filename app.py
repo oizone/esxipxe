2d9c74cdcfe7bf267fd0e70d3aaf8630c826e9e3
Onni Rautanen <oizone@oizone.net>
2020-07-22 20:57:44 +0300

    ap

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+    start_response('200 OK', [('Content-Type','text/plain')])
+    #open
+    return [env]
