ca46ada9a6cfcedb08651ea465d8b650d3fe79fc
Onni Rautanen <oizone@oizone.net>
2020-07-22 12:21:06 +0300

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:latest
 RUN apk add --no-cache dhcp tftp-hpa nginx git p7zip py3-pip
 RUN pip3 install xlrd
-RUN mkdir -p /tftp/cd
+#RUN mkdir -p /tftp/cd
 RUN touch /var/lib/dhcp/dhcpd.leases
 RUN mkdir /run/nginx
 COPY entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+git clone https://github.com/oizone/esxipxe.git /tftp
+mkdir /tftp/cd
 7z x -o/tftp/cd /tftp/ESXi.iso
 dhcpd -4 
 /usr/sbin/in.tftpd --listen --address 0.0.0.0:69 --secure --retransmit 1000000 --blocksize 512 -vvv /tftp
+nginx
