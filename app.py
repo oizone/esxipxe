3d62aef0886ba72f519171f3f7743f3e860195c3
Onni Rautanen <oizone@oizone.net>
2020-07-22 22:40:21 +0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+import os
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/plain')])
-    if(env['PATH_INFO'].split('/')[1]=="cd"):
-        return ["JOO CEEDEE"]
+    filename="/tftp"+env['PATH_INFO']
+    if(os.path.isfile(filename)):
+       if(env['PATH_INFO'].split('/')[1]=="cd"):
+           start_response('200 OK', [('Content-Type','application/x-binary')])
+           return ["JOO CEEDEE"]
+           file=open(filename,'r')
+           return [file.read()]
+       else:
+           start_response('200 OK', [('Content-Type','text/plain')])
+           file=open(filename,'r')
+           return [file.read()]
     else:
-        file=open("/tftp"+env['PATH_INFO'],'r')
-        return [file.read()]
+        start_response('404 Not Found',[('Content-Type','text/plain')])
+        return ["Not Found"]
+        
