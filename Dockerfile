adff35fa8ce2e085e157434b7e6239eef1e16b39
Onni Rautanen <oizone@oizone.net>
2020-07-22 13:51:33 +0300

    update image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,26 @@
-FROM alpine:latest
-RUN apk add --no-cache dhcp tftp-hpa nginx git p7zip py3-pip
-RUN pip3 install xlrd
-#RUN mkdir -p /tftp/cd
-RUN touch /var/lib/dhcp/dhcpd.leases
-RUN mkdir /run/nginx
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1
+LABEL maintainer="oizone@oizone.net"
+
+ARG GH_RUNNER_VERSION="2.267.1"
+ARG TARGETPLATFORM
+
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+ENV DEBIAN_FRONTEND=noninteractive
+ENV ANSIBLE_HOST_KEY_CHECKING=False
+ARG APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=yes
+
+RUN echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | tee -a /etc/apt/sources.list
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+RUN apt-get update
+RUN apt-get install -y --no-install-recommends jq python-ncclient ansible
+
+WORKDIR /actions-runner
+COPY install_actions.sh /actions-runner
+
+RUN chmod +x /actions-runner/install_actions.sh \
+  && /actions-runner/install_actions.sh ${GH_RUNNER_VERSION} ${TARGETPLATFORM} \
+  && rm /actions-runner/install_actions.sh
+
 COPY entrypoint.sh /
 RUN chmod +x /entrypoint.sh
-#ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,50 @@
-#!/bin/sh
-git clone https://github.com/oizone/esxipxe.git /tftp
-mkdir /tftp/cd
-7z x -o/tftp/cd /ESXi.iso
-dhcpd -4 
-/usr/sbin/in.tftpd --listen --address 0.0.0.0:69 --secure --retransmit 1000000 --blocksize 512 -vvv /tftp
-nginx
+#!/bin/bash
+
+export RUNNER_ALLOW_RUNASROOT=1
+export PATH=$PATH:/actions-runner
+
+_RUNNER_NAME=${RUNNER_NAME:-default}
+_RUNNER_WORKDIR=${RUNNER_WORKDIR:-/_work}
+_ORG_RUNNER=${ORG_RUNNER:-false}
+_LABELS=${LABELS:-default}
+
+if [[ -n "${ACCESS_TOKEN}" ]]; then
+    URI=https://api.github.com
+    API_VERSION=v3
+    API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+    AUTH_HEADER="Authorization: token ${ACCESS_TOKEN}"
+
+    _PROTO="$(echo "${REPO_URL}" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    # shellcheck disable=SC2116
+    _URL="$(echo "${REPO_URL/${_PROTO}/}")"
+    _PATH="$(echo "${_URL}" | grep / | cut -d/ -f2-)"
+    _ACCOUNT="$(echo "${_PATH}" | cut -d/ -f1)"
+    _REPO="$(echo "${_PATH}" | cut -d/ -f2)"
+
+    _FULL_URI="${URI}/repos/${_ACCOUNT}/${_REPO}/actions/runners/registration-token"
+    _SHORT_URL="${REPO_URL}"
+    if [[ ${_ORG_RUNNER} == "true" ]]; then
+      [[ -z ${ORG_NAME} ]] && ( echo "ORG_NAME required for org runners"; exit 1 )
+      _FULL_URI="${URI}/orgs/${ORG_NAME}/actions/runners/registration-token"
+      _SHORT_URL="${_PROTO}github.com/${ORG_NAME}"
+    fi
+    echo curl -XPOST -fsSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${_FULL_URI}"
+    RUNNER_TOKEN="$(curl -XPOST -fsSL \
+      -H "${AUTH_HEADER}" \
+      -H "${API_HEADER}" \
+      "${_FULL_URI}" \
+    | jq -r '.token')"
+fi
+
+echo "Configuring"
+
+./config.sh \
+    --url "${_SHORT_URL}" \
+    --token "${RUNNER_TOKEN}" \
+    --name "${_RUNNER_NAME}" \
+    --work "${_RUNNER_WORKDIR}" \
+    --labels "${_LABELS}" \
+    --unattended \
+    --replace
+
+./run.sh
